9e2fe704e4332e24f4894e476a56b87049c99e4d
riday-kumar <hridoykumarsaha07@gmail.com>
2026-08-03 13:26:31 +0600

    fix: update next js config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.ibb.co",
       },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co.com",
+      },
     ],
   },
 };
